999c9ff0d564853dcd9b93a2a00f26078c783d28
PabloChimi <Pablo.chimi@hotmail.com>
2018-06-22 06:53:10 -0300

    Tests

---
+++ Trabajo Practico Nº 4/CorreoUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CorreoUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CorreoUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("619c3ab2-7b88-4295-85c3-656293d1c874")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Trabajo Practico Nº 4/CorreoUnitTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+
+namespace CorreoUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void InicializacionCorreoListPaquetes()
+        {
+            Correo c = new Correo();
+            Assert.IsNotNull(c.Paquetes);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void TestPaquetesNoRepetidos()
+        {
+            Correo c = new Correo();
+
+            Paquete p1 = new Paquete("Gerli", "000-000-0000");
+            Paquete p2 = new Paquete("Palermo", "000-000-0000");
+            
+            c = c + p1;
+            c = c + p2;
+        }
+    }
+}
